a6442ed0f1e90e34c34956ac0f89e4f399a40907
supratikpaul <supratik.dev04@gmail.com>
2025-12-04 13:38:45 +0530

    Update expapp2.py

--- expapp2.py
+++ expapp2.py
@@ -186,7 +186,7 @@ def add_expense():
             total = sum(float(item["amount"]) for item in expense) if expense else 0
             return render_template(
                 #"exptracker2.html",
-                "add_expense.html"
+                "add_expense.html",
                 expense=expense,
                 total=total,
                 error="Amount and category are required"
